4a6bd00042e16dbcbd602521a7df0c57e9dda19a
Dmitruk Misha <37069153+DmitrukMisha@users.noreply.github.com>
2020-04-24 16:01:30 +0300

    Add files via upload

---
+++ footer.js
@@ -0,0 +1,17 @@
+document.write(`
+
+<div class="footer" id="importfooter">	
+<div style="float: left; margin-left: 5%;">
+			
+				<a href="mailto:info@sportzaldoma.by">info@sportzaldoma.by</a>
+				<br>
+				<a href="tel:+375291959090">тел +375 (29) 195-90-90</a>  <a href="tel:+375297800990">тел +375 (29) 780-09-90</a> 
+</div>				
+<div id="footerP">
+				<p >г. Минск, пр-т Независимости д. 177, пом. 3, комната 22<br>
+				 Время работы: круглосуточно <br>
+				 Без выходных</p> 
+</div>			
+</div>
+
+`);
---
+++ header.js
@@ -0,0 +1,47 @@
+document.write(`
+
+   <!-- Yandex.Metrika counter -->
+<script type="text/javascript" >
+   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+   m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+   ym(61894266, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true,
+        webvisor:true
+   });
+</script>
+<noscript><div><img src="https://mc.yandex.ru/watch/61894266" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
+<div class="header" >
+<a href ="http://sportzaldoma.by"><img id="LOGO" alt="Логотип sportzaldoma.by" src="images/logo11.png" title="Главная страница"></a>
+<p><a style=" color: white; padding-right: 10px"href="tel:+375291959090">+375 (29) 195-90-90</a>   &#160;  &#160;  &#160;  &#160;
+      <a style=" color: white;"href="tel:+375297800990">+375 (29) 780-09-90</a> &#160;  &#160;</p>
+      <div id="GreyLine"></div>
+      <div class="header_menu" >  
+           <section> 
+          <a href="http://sportzaldoma.by">Главная</a>
+          <a href="About.html">О компании</a>
+          <a href="Products.html"><u>Каталог</u></a>
+        <a href="Contacts.html">Контакты</a> 
+        </section>
+
+          <aside> 
+          <span id="sale"><h1>ПРОКАТ СПОРТИВНЫХ ТРЕНАЖЕРОВ В МИНСКЕ</h1></span>
+           </aside>
+          <input  type="checkbox" name="menu" id="btn-menu" />
+           <label  for="btn-menu">Меню</label> 
+           <div id="menu">
+        <div class="header_menu"><a href="http://sportzaldoma.by">Главная</a></div>
+        <div class="header_menu"><a href="About.html">О компании</a></div>
+        <div class="header_menu"><a href="Products.html"><u>Каталог</u></a></div>
+        <div class="header_menu"><a href="Contacts.html">Контакты</a></div>
+      </div>
+      </div>
+
+      
+</div>
+
+`);
