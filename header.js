ea6614cf0a38624d22d4bbd0e2020d72f4c3575d
Dmitruk Misha <37069153+DmitrukMisha@users.noreply.github.com>
2020-04-30 21:07:49 +0300

    Add files via upload

--- header.js
+++ header.js
@@ -2,16 +2,16 @@ document.write(`
 
 
 <div class="header" >
-<a href ="http://sportzaldoma.by"><img id="LOGO" alt="Логотип sportzaldoma.by" src="images/logo11.png" title="Главная страница"></a>
+<a href ="http://sportzaldoma.by"><img id="LOGO" alt="Логотип sportzaldoma.by" src="http://sportzaldoma.by/images/logo11.png" title="Главная страница"></a>
 <p><a style=" color: white; padding-right: 10px"href="tel:+375291959090">+375 (29) 195-90-90</a>   &#160;  &#160;  &#160;  &#160;
       <a style=" color: white;"href="tel:+375297800990">+375 (29) 780-09-90</a> &#160;  &#160;</p>
       <div id="GreyLine"></div>
       <div class="header_menu" >  
            <section> 
           <a href="http://sportzaldoma.by">Главная</a>
-          <a href="About.html">О компании</a>
-          <a href="Products.html"><u>Каталог</u></a>
-        <a href="Contacts.html">Контакты</a> 
+          <a href="http://sportzaldoma.by/About.html">О компании</a>
+          <a href="http://sportzaldoma.by/Products.html"><u>Каталог</u></a>
+        <a href="http://sportzaldoma.by/Contacts.html">Контакты</a> 
         </section>
 
           <aside> 
@@ -21,9 +21,9 @@ document.write(`
            <label  for="btn-menu">Меню</label> 
            <div id="menu">
         <div class="header_menu"><a href="http://sportzaldoma.by">Главная</a></div>
-        <div class="header_menu"><a href="About.html">О компании</a></div>
-        <div class="header_menu"><a href="Products.html"><u>Каталог</u></a></div>
-        <div class="header_menu"><a href="Contacts.html">Контакты</a></div>
+        <div class="header_menu"><a href="http://sportzaldoma.by/About.html">О компании</a></div>
+        <div class="header_menu"><a href="http://sportzaldoma.by/Products.html"><u>Каталог</u></a></div>
+        <div class="header_menu"><a href="http://sportzaldoma.by/Contacts.html">Контакты</a></div>
       </div>
       </div>
 
